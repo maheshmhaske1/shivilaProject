a022b6150301f68cae4d7c6067b71241b8f806c8
Mahesh mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2023-04-10 14:45:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const { DB_STRING } = process.env
-
-exports.createConnection = () => {
-    mongoose.connect(`mongodb+srv://dipvadukiya06:S5lxmxRcgxX7pncH@cluster0.6bj6yrx.mongodb.net/Crypto`, (error) => {
-        if (error) {
-            console.log('error while connecting with database')
+exports.dbConnection = async () => {
+    mongoose.connect("mongodb+srv://dipvadukiya06:S5lxmxRcgxX7pncH@cluster0.6bj6yrx.mongodb.net/Crypto", (error) => {
+        if (!error) {
+            console.log('Connected with MongoDB...')
         }
         else {
-            console.log('connected with database')
+            console.log('error while connecting...', error)
         }
     })
 }
