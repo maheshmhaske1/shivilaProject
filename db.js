a68323a47346742aa11c4299b30f37cfecd747e8
Mahesh mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2023-04-10 14:44:14 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const { DB_STRING } = process.env
 
 exports.createConnection = () => {
-    mongoose.connect(`mongodb://mahesh:Mahesh3332@cluster0-shard-00-00.kjb1q.mongodb.net:27017,cluster0-shard-00-01.kjb1q.mongodb.net:27017,cluster0-shard-00-02.kjb1q.mongodb.net:27017/?ssl=true&replicaSet=atlas-140sev-shard-0&authSource=admin&retryWrites=true&w=majority`, (error) => {
+    mongoose.connect(`mongodb+srv://dipvadukiya06:S5lxmxRcgxX7pncH@cluster0.6bj6yrx.mongodb.net/Crypto`, (error) => {
         if (error) {
             console.log('error while connecting with database')
         }
